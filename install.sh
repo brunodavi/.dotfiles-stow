54670b05e02884d2c217d3c1b8540811d30a96ae
brunodavi <48166556+brunodavi@users.noreply.github.com>
2026-09-05 19:22:33 -0300

    feat: agnostic install

--- install.sh
+++ install.sh
@@ -11,20 +11,33 @@ APPS=(
 CONFIGS=(
   git
   nvim
-  bash
 )
 
 has_cmd() {
   command -v "$1" &>/dev/null
 }
 
-if has_cmd apk; then
-  apk add --no-cache "${APPS[@]}"
-elif has_cmd pkg; then
-  pkg install -y "${APPS[@]}"
-elif has_cmd apt-get; then
-  sudo apt-get install -y "${APPS[@]}"
+check_all() {
+    local all_ok=0
+    for app in "${APPS[@]}"; do
+        has_cmd "${app}" || all_ok=1
+    done
+
+    return all_ok
+}
+
+if [ ! check_all ]; then 
+    if has_cmd apk; then
+      apk add --no-cache "${APPS[@]}"
+    elif has_cmd pkg; then
+      pkg install -y "${APPS[@]}"
+    elif has_cmd apt-get; then
+      sudo apt-get install -y "${APPS[@]}"
+    fi
+else
+    echo 'All installed'
 fi
+
 
 mkdir -p ~/.config
 
@@ -32,3 +45,15 @@ for cfg in "${CONFIGS[@]}"; do
   echo "Link: $cfg"
   stow -R "$cfg"
 done
+
+DOTFILES_INIT='~/.dotfiles/shell/init.sh'
+
+CONTAIN_INIT=0
+grep -qw "${DOTFILES_INIT}" ~/.bashrc || CONTAIN_INIT=1
+
+if [ ${CONTAIN_INIT} = 1 ]; then
+    echo "Add init.sh on .bashrc"
+    echo -e "\nsource ${DOTFILES_INIT}" >> ~/.bashrc
+else
+    echo "Always init.sh on .bashrc"
+fi

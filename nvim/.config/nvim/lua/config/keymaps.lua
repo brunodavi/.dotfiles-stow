5ce05ffe38ab09c5652fe924d27aea1bebbf3bed
brunodavi <48166556+brunodavi@users.noreply.github.com>
2026-09-10 23:33:49 -0300

    add descriptions to all keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,12 @@
 local map = vim.keymap.set
 
-map("i", "jk", "<Esc>", { noremap = true })
+map("i", "jk", "<Esc>", { noremap = true, desc = "exit insert mode" })
 
-map("n", "<Esc>", "<cmd>noh<CR>", { silent = true })
+map("n", "<Esc>", "<cmd>noh<CR>", { silent = true, desc = "clear search highlight" })
 
-map("n", "<leader>q", "<cmd>q<CR>")
-map("n", "<leader>w", "<cmd>w<CR>")
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "quit buffer" })
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "save buffer" })
 
-map("n", "q", "<Nop>")
+map("n", "q", "<Nop>", { desc = "disable single q" })
 
-map({ "n", "v" }, "<leader>y", '"+y')
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "copy to system clipboard" })
